ed75dd5f4ff86a626947e8003bcec209e5dcd1ff
xyryc <amdanikzz@gmail.com>
2024-11-04 03:21:36 +0600

    dynamically fetched data

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,7 +10,9 @@ const Categories = ({ categories }) => {
           to={`/category/${category.category}`}
           role="tab"
           className={({ isActive }) =>
-            `btn rounded-[32px] ${isActive ? "bg-[#9538E2] text-white" : "text-gray-600"}`
+            `btn rounded-[32px] ${
+              isActive ? "bg-[#9538E2] text-white" : "text-gray-600"
+            }`
           }
         >
           {category.category}
--- src/components/ProductCards.jsx
+++ src/components/ProductCards.jsx
@@ -1,10 +1,38 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const ProductCards = () => {
-  const { category } = useParams();
-  console.log(category);
+  const [products, setProducts] = useState([]);
 
-  return <div>product</div>;
+  const { category } = useParams();
+  // console.log(typeof category);
+  const data = useLoaderData();
+  // console.log(data)
+
+  useEffect(() => {
+    if (category) {
+      if (category === "All Product") {
+        setProducts(data.slice(0,9));
+      } else {
+        const filteredCategory = [...data].filter(
+          (coffee) => coffee.category === category
+        );
+        setProducts(filteredCategory);
+      }
+    } else {
+      setProducts(data.slice(0,9));
+    }
+  }, [category, data]);
+
+  console.log(products);
+
+  return (
+    <div>
+      {products.map((product) => (
+        <div key={product.product_id}>{product.product_title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductCards;
