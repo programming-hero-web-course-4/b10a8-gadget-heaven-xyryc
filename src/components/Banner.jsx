b472e455487fa810964e212606ef70e9d7f58208
xyryc <amdanikzz@gmail.com>
2024-11-03 22:34:46 +0600

    added banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,28 @@
+import BannerImage from "../assets/banner.jpg";
 
 const Banner = () => {
-    return (
-        <div>
-            Banner
-        </div>
-    );
+  return (
+    <div className="relative">
+      <div className="bg-[#9538E2]  text-center text-white pt-[118px]  pb-[268px] rounded-[32px] px-2 mb-36 sm:mb-56 lg:mb-[490px]">
+        <h1 className="text-[56px] font-bold mb-6">
+          Upgrade Your Tech Accessorize with Gadget Heaven Accessories
+        </h1>
+        <p className="mb-8">
+          Explore the latest gadgets that will take your experience to the next
+          level. From smart devices to the coolest accessories, we have it all!
+        </p>
+        <button className="btn rounded-[32px] text-[#9538E2]">Shop Now</button>
+      </div>
+
+      <div className="absolute -bottom-28 sm:-bottom-40 md:-bottom-56 lg:-bottom-80 2xl:-bottom-[500px] rounded-[32px] p-6 border mx-4 lg:mx-52">
+        <img
+          className="rounded-3xl object-cover"
+          src={BannerImage}
+          alt="banner image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,17 @@ import Banner from "../components/Banner";
 import Categories from "../components/Categories";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner/>
+  return (
+    <div className="container mx-auto px-4">
+      <Banner />
 
-            <h2 className="font-bold text-[40px] text-center">Explore Cutting-Edge Gadgets</h2>
+      <h2 className="font-bold text-[40px] text-center">
+        Explore Cutting-Edge Gadgets
+      </h2>
 
-            <Categories/>
-
-            
-        </div>
-    );
+      <Categories />
+    </div>
+  );
 };
 
 export default Home;
